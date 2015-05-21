e2c67b6f17b4ff374151478237c40511721b70bb
Gig <thegigabit101@gmail.com>
2015-05-22 00:37:46 +0100

    Format Version bump

--- src/main/java/BaublesHud/client/HudBaubles.java
+++ src/main/java/BaublesHud/client/HudBaubles.java
@@ -1,20 +1,17 @@
 package BaublesHud.client;
 
-import org.lwjgl.opengl.GL11;
-import org.omg.CORBA.PRIVATE_MEMBER;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+
+import org.lwjgl.opengl.GL11;
+
 import baubles.common.container.InventoryBaubles;
 import baubles.common.lib.PlayerHandler;
 import cpw.mods.fml.common.eventhandler.EventPriority;
@@ -22,79 +19,83 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class GuiBaubleHud{
-	
-	public static GuiBaubleHud instancemain = new GuiBaubleHud();
+public class HudBaubles {
+
+	public static HudBaubles instancemain = new HudBaubles();
 	private static Minecraft mc = Minecraft.getMinecraft();
-    public static int LocX = 1;
-    public static int LocY = 1;
-    private int guiLeft, guiTop;
-	
+	public static int LocX = 1;
+	public static int LocY = 1;
+	private int guiLeft, guiTop;
+
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public void onRenderExperienceBar(RenderGameOverlayEvent event) 
 	{
-	    if(event.isCancelable() || event.type != ElementType.ALL)
-	    {      
-	      return;
-	    }
-		
+		if (event.isCancelable() || event.type != ElementType.ALL) 
+		{
+			return;
+		}
+
 		if (mc.inGameHasFocus || (mc.currentScreen != null && mc.gameSettings.showDebugInfo)) 
-		{		
-			drawbaublesHud(event.resolution);		
+		{
+			drawbaublesHud(event.resolution);
 		}
 
 	}
-	
-	public void drawbaublesHud(ScaledResolution res)
+
+	public void drawbaublesHud(ScaledResolution res) 
 	{
 		EntityPlayer player = mc.thePlayer;
 		InventoryBaubles inv = PlayerHandler.getPlayerBaubles(player);
-		
+
 		ItemStack stack0 = inv.getStackInSlot(0);
-		if (stack0 !=null)
+		if (stack0 != null) 
 		{
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 			GL11.glDisable(GL11.GL_LIGHTING);
 			Item item0 = stack0.getItem();
 			IIcon icon1 = item0.getIconFromDamage(0);
 
-			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine, new ItemStack (item0), LocX + 5, LocY + 100);
+			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine,
+							new ItemStack(item0), LocX + 5, LocY + 100);
 		}
-		
+
 		ItemStack stack1 = inv.getStackInSlot(1);
-		if (stack1 !=null)
+		if (stack1 != null) 
 		{
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 			GL11.glDisable(GL11.GL_LIGHTING);
 			Item item1 = stack1.getItem();
 			IIcon icon1 = item1.getIconFromDamage(0);
 
-			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine, new ItemStack (item1), LocX + 5, LocY + 120);
+			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine,
+							new ItemStack(item1), LocX + 5, LocY + 120);
 		}
-		
+
 		ItemStack stack2 = inv.getStackInSlot(2);
-		if (stack2 !=null)
+		if (stack2 != null) 
 		{
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 			GL11.glDisable(GL11.GL_LIGHTING);
 			Item item2 = stack2.getItem();
 			IIcon icon2 = item2.getIconFromDamage(0);
 
-			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine, new ItemStack (item2), LocX + 5, LocY + 140);
+			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine,
+							new ItemStack(item2), LocX + 5, LocY + 140);
 		}
-		
+
 		ItemStack stack3 = inv.getStackInSlot(3);
-		if (stack3 !=null)
+		if (stack3 != null) 
 		{
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 			GL11.glDisable(GL11.GL_LIGHTING);
 			Item item3 = stack3.getItem();
 			IIcon icon3 = item3.getIconFromDamage(0);
 
-			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine, new ItemStack (item3), LocX + 5, LocY + 160);
+			RenderItem.getInstance().renderItemIntoGUI(mc.fontRenderer, mc.renderEngine,
+							new ItemStack(item3), LocX + 5, LocY + 160);
 		}
-		
+
 	}
 
 }
--- src/main/java/BaublesHud/proxy/ClientProxy.java
+++ src/main/java/BaublesHud/proxy/ClientProxy.java
@@ -1,17 +1,15 @@
 package BaublesHud.proxy;
 
-import BaublesHud.client.GuiBaubleHud;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.common.MinecraftForge;
 
-public class ClientProxy extends CommonProxy{
-	
+public class ClientProxy extends CommonProxy {
+
 	private Minecraft mc = Minecraft.getMinecraft();
-	
+
 	@Override
-	public void registerRenders()
+	public void registerRenders() 
 	{
-//		MinecraftForge.EVENT_BUS.register(new GuiBaubleHud(mc));
+		
 	}
 
 }
