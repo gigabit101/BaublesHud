918f73ced0b92921ce8543b5fae5d1d220f5be7d
Ruben Castaneda <rubennc1994@gmail.com>
2015-05-26 22:22:40 -0700

    Cleanup
    Small Rewrites and Code Cleanups and Minor Error Fixes

--- src/main/java/BaublesHud/client/HudBaubles.java
+++ src/main/java/BaublesHud/client/HudBaubles.java
@@ -21,12 +21,11 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class HudBaubles {
-
+public class HudBaubles
+{
 	public static ConfigBaublesHud config;
 	public static HudBaubles instancemain = new HudBaubles();
 	private static Minecraft mc = Minecraft.getMinecraft();
-	private static ScaledResolution scaledResolution;
 	public static KeyBindings key;
 	public static int LocX;
 	public static int LocY;
@@ -38,110 +37,63 @@ public class HudBaubles {
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public void onRenderExperienceBar(RenderGameOverlayEvent event) 
 	{
-		if (event.isCancelable() || event.type != ElementType.ALL) 
-		{
+		if (event.isCancelable() || event.type != ElementType.ALL)
 			return;
-		}
-		
-		if (key.config.isPressed() == true)
-		{
-			// Adds 1 to the current screen position when Keybinding is pressed.
-			if(hudPosition !=7)
-			{
-				hudPosition += 1;
-			}
-			// If current Keybinding position is 7 resets it to 0
-			else if(hudPosition == 7)
-			{
-				hudPosition = 0;
-			}
-		}
-			// Determines where to put the Hud depending on int
-			if(hudPosition == 0)
-			{
-				LocX = 1;
-				LocY = 1;
-				LocOffsetX = LocX + 15;
-				LocOffsetY = 0;
-			}
-			else if(hudPosition == 1)
-			{
-				LocX = 1;
-				LocX = 1;
-				LocOffsetX = 0;
-				LocOffsetY = LocY + 15;
-			}
-			else if (hudPosition == 2)
-			{
-				LocX = event.resolution.getScaledWidth() - 60;
-				LocY = 1;
-				LocOffsetX =  15;
-				LocOffsetY = 0;
-			}
-			else if (hudPosition == 3)
-			{
-				LocX = event.resolution.getScaledWidth() - 20;
-				LocY = 1;
-				LocOffsetX = 0;
-				LocOffsetY = LocY + 15;
-			}
-			else if (hudPosition == 4)
-			{
-				LocX = 1;
-				LocY = event.resolution.getScaledHeight() - 20;
-				LocOffsetX = LocX + 15;
-				LocOffsetY = 0;
-			}
-			else if (hudPosition == 5)
-			{
-				LocX = 1;
-				LocY = event.resolution.getScaledHeight() - 60;
-				LocOffsetX = 0;
-				LocOffsetY = 15;
-			}
-			else if (hudPosition == 6)
-			{
-				LocX = event.resolution.getScaledWidth() - 60;
-				LocY = event.resolution.getScaledHeight() - 20;
-				LocOffsetX = 15;
-				LocOffsetY = 0;
-			}
-			else if (hudPosition == 7)
-			{
-				LocX = event.resolution.getScaledWidth() - 20;
-				LocY = event.resolution.getScaledHeight() - 60;
-				LocOffsetX = 0;
-				LocOffsetY = 15;
-			}
 
-		if (mc.inGameHasFocus || (mc.currentScreen != null && mc.gameSettings.showDebugInfo)) 
-		{
-			scaledResolution = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
-			drawbaublesHudIcons(event.resolution);
+		// Adds 1 to the current screen position when Keybinding is pressed.
+		if (key.config.isPressed() == true)
+			hudPosition = (hudPosition + 1) % 8;
+
+		// Following Code Determines HUD Position
+		
+		// Setup Flags
+		boolean isOnLeft = ((hudPosition / 2) % 2 == 0);
+		boolean isOnTop = hudPosition <= 3;
+		boolean isHorz = hudPosition % 2 == 0;
+
+		// Left/Right Side Check
+		if (isOnLeft)
+			LocX = 1;
+		else
+			LocX = event.resolution.getScaledWidth() - 15;
+
+		// Top/Bottom Check
+		if (isOnTop)
+			LocY = 1;
+		else
+			LocY = event.resolution.getScaledHeight() - 15;
+
+		// Following Code Sets Up Offsets. Determines HUD Orientation
+		if (isHorz) { // Horizontal HUD
+			LocOffsetX = (isOnLeft ? 15 : -15);
+			LocOffsetY = 0;
+		} else { // Vertical HUD
+			LocOffsetX = 0;
+			LocOffsetY = (isOnTop ? 15 : -15);
 		}
+
+		if (mc.inGameHasFocus || (mc.currentScreen != null && mc.gameSettings.showDebugInfo))
+			drawbaublesHudIcons(event.resolution);
 		
 	}
+
 	// Draws the Hud
 	public void drawbaublesHudIcons(ScaledResolution res) 
 	{
 		EntityPlayer player = mc.thePlayer;
 		InventoryBaubles inv = PlayerHandler.getPlayerBaubles(player);
-		//Gets the ItemStack from the players baubles inventory.
-		ItemStack stack0 = inv.getStackInSlot(0);
-		ItemStack stack1 = inv.getStackInSlot(1);
-		ItemStack stack2 = inv.getStackInSlot(2);
-		ItemStack stack3 = inv.getStackInSlot(3);
-		// Renders the ItemStacks icon to the hud in the correct X, Y positions 
-		renderItemstack(mc, stack0, LocX , LocY);
-		renderItemstack(mc, stack1, LocX + LocOffsetX, LocY + LocOffsetY);
-		renderItemstack(mc, stack2, LocX + LocOffsetX + LocOffsetX, LocY + LocOffsetY + LocOffsetY);
-		renderItemstack(mc, stack3, LocX + LocOffsetX + LocOffsetX + LocOffsetX, LocY + LocOffsetY + LocOffsetY + LocOffsetY);
+
+		// Renders the ItemStacks from the players baubles inventory in the correct
+		//	X, Y Cordinates
+		for (int i = 0; i < 4; i++)
+			renderItemstack(inv.getStackInSlot(i), LocX + i * LocOffsetX,
+				LocY + i * LocOffsetY);
 	}
 	
-	public void renderItemstack(Minecraft mc, ItemStack stack, int x, int y)
+	// Draws ItemStack at X and Y Cordinates
+	public void renderItemstack(ItemStack stack, int x, int y)
 	{
-		if (stack != null)
-		{
+		if (stack != null) {
 			// Renders Item Icon.
 			RenderItem.getInstance().renderItemAndEffectIntoGUI(mc.fontRenderer, mc.renderEngine, stack, x, y);
 			// Renders Item Overlay example durability bar
